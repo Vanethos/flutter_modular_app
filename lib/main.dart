3e8bf674b0072ecc324ca84beca7fe51c4df6c7b
Gonçalo Palma <solid.goncalo@gmail.com>
2019-11-10 12:25:47 +0400

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular_app/presentation/ui/app.dart';
+
+void main() => runApp(MyApp());
+
+
---
+++ lib/presentation/ui/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular_app/presentation/ui/home/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
